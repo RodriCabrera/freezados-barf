2657e039e8f29e75575e2c1fab8daadb1e643308
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-07-29 17:16:59 -0300

    feat: add prettier and change configurations to run yarn lint or yarn lint:fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,44 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript',
+    'prettier'
+  ],
   overrides: [],
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
   },
-  plugins: ["react", "react-native"],
-  ignorePatterns: ["node_modules/", "build/", "dist/", ".eslintrc.js"],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  ignorePatterns: ['node_modules/', 'build/', 'dist/', '.eslintrc.js'],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "warn",
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/naming-convention": "warn",
-    "@typescript-eslint/await-thenable": "warn",
-  },
-};
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/naming-convention': 'warn',
+    '@typescript-eslint/await-thenable': 'warn',
+    'react-native/no-raw-text': 'off'
+  }
+}
