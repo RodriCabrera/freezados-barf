1467a935a7fcce18fea6af3ac8bdb73b93eb86ee
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-07 17:34:36 -0300

    format files

--- components/DAOTests.tsx
+++ components/DAOTests.tsx
@@ -6,86 +6,106 @@ import UbicationDAO from '../database/DAOs/Ubication.dao'
 import EntryDAO from '../database/DAOs/Entry.dao'
 
 function prepare() {
-    const users = new UserDAO()
-    users.dropTable('USER')
-    const ubications = new UbicationDAO()
-    ubications.dropTable('UBICATION')
-    const foods = new FoodDAO()
-    foods.dropTable('FOOD')
-    const entries = new EntryDAO()
-    entries.dropTable('ENTRY')
+  const users = new UserDAO()
+  users.dropTable('USER')
+  const ubications = new UbicationDAO()
+  ubications.dropTable('UBICATION')
+  const foods = new FoodDAO()
+  foods.dropTable('FOOD')
+  const entries = new EntryDAO()
+  entries.dropTable('ENTRY')
 }
 
 // prepare() // -> Prueba que se creen correctamente las tablas si ya estabamos usando alguna
 
 export function DAOTests() {
+  const [userId, setUserId] = useState<number>()
+  const [ubicationId, setUbicationId] = useState<number>()
+  const [foodId, setFoodId] = useState<number>()
+  const [entryId, setEntryId] = useState<number>()
 
-    const [userId, setUserId] = useState<number>()
-    const [ubicationId, setUbicationId] = useState<number>()
-    const [foodId, setFoodId] = useState<number>()
-    const [entryId, setEntryId] = useState<number>()
+  useEffect(() => {
+    const users = new UserDAO()
+    users.deleteAll('USER')
+    users.insertOne({ name: 'Roberto Gomez Bolaños' }).then((res) => {
+      if (res) {
+        setUserId(res)
+        users.getById(res).then((newUser) => {
+          console.log('\nUsuario agregado', newUser)
+        })
+      }
+    })
+  }, [])
 
-    useEffect(() => {
-        const users = new UserDAO()
-        users.deleteAll('USER')
-        users.insertOne({name: 'Roberto Gomez Bolaños'}).then(res => {
-            if(res) {
-                setUserId(res)
-                users.getById(res).then(newUser => console.log("\nUsuario agregado", newUser))
-            }
-        })
-    }, [])
+  useEffect(() => {
+    if (!userId) return
+    const ubications = new UbicationDAO()
+    ubications.deleteAll('UBICATION')
+    const foods = new FoodDAO()
+    foods.deleteAll('FOOD')
+    ubications
+      .insertOne({
+        user_id: userId,
+        description: 'Freezer de casa',
+        isFreezer: true,
+        name: 'Freezer',
+        ubication: 'Casa'
+      })
+      .then((res) => {
+        if (res) {
+          setUbicationId(res)
+          ubications.getById(res).then((newUb) => {
+            console.log('\nUbicacion agregada: ', newUb)
+          })
+        }
+      })
+    foods
+      .insertOne({
+        name: 'Pechuga de pollo',
+        species: 'AVIAR',
+        user_id: userId,
+        description: 'Pechuga del coto'
+      })
+      .then((res) => {
+        if (res) {
+          setFoodId(res)
+          foods.getById(res).then((newFood) => {
+            console.log('\nAlimento agregado: ', newFood)
+          })
+        }
+      })
+  }, [userId])
 
-    useEffect(() => {
-        if (!userId) return
-        const ubications = new UbicationDAO()
-        ubications.deleteAll('UBICATION')
-        const foods = new FoodDAO()
-        foods.deleteAll('FOOD')
-        ubications.insertOne(
-            {user_id: userId, description: 'Freezer de casa', isFreezer: true, name: 'Freezer', ubication: 'Casa'}
-        ).then(res => {
-            if (res) { 
-                setUbicationId(res)
-                ubications.getById(res).then(newUb => console.log("\nUbicacion agregada: ", newUb))
-            }
-        })
-        foods.insertOne(
-            {name: 'Pechuga de pollo', species: 'AVIAR', user_id: userId, description: 'Pechuga del coto'}
-        ).then(res => {
-            if (res) {
-                setFoodId(res)
-                foods.getById(res).then(newFood => console.log("\nAlimento agregado: ", newFood))
-            }
-        })
-    }, [userId])
-
-    useEffect(() => {
-        if (!userId || !foodId || !ubicationId) return
-        const entries = new EntryDAO()
-        entries.deleteAll('ENTRY')
-        entries.insertOne({
-            user_id: userId,
-            ubication_id: ubicationId,
-            food_id: foodId,
-            date_stored: undefined,
-            date_ready: undefined,
-            quantity: 1500,
-            taken: false
-        }).then(res => {
-            if (res) {
-                setEntryId(res)
-                entries.getById(res).then(newEntry => console.log("\nNuevo elemento guardado: ", newEntry))
-            }
-        })
-    }, [userId, foodId, ubicationId])
+  useEffect(() => {
+    if (!userId || !foodId || !ubicationId) return
+    const entries = new EntryDAO()
+    entries.deleteAll('ENTRY')
+    entries
+      .insertOne({
+        user_id: userId,
+        ubication_id: ubicationId,
+        food_id: foodId,
+        date_stored: undefined,
+        date_ready: undefined,
+        quantity: 1500,
+        taken: false
+      })
+      .then((res) => {
+        if (res) {
+          setEntryId(res)
+          entries.getById(res).then((newEntry) => {
+            console.log('\nNuevo elemento guardado: ', newEntry)
+          })
+        }
+      })
+  }, [userId, foodId, ubicationId])
 
   return (
     <View>
-        <Text>User id: {userId}</Text>
-        <Text>Ubication id: {ubicationId}</Text>
-        <Text>Food id: {foodId}</Text>
-        <Text>Entry id: {entryId}</Text>
+      <Text>User id: {userId}</Text>
+      <Text>Ubication id: {ubicationId}</Text>
+      <Text>Food id: {foodId}</Text>
+      <Text>Entry id: {entryId}</Text>
     </View>
-    )
+  )
 }
--- database/DAOs/User.dao.ts
+++ database/DAOs/User.dao.ts
@@ -1,44 +1,56 @@
-import BaseDAO from "./Base.dao";
-import SQLiteDB from "../SQLite.database";
+import BaseDAO from './Base.dao'
+import SQLiteDB from '../SQLite.database'
 
-export type User = {
-    id: number
-    name?: string
-    email?: string
+export interface User {
+  id: number
+  name?: string
+  email?: string
 }
 
 const TABLE_NAME = 'USER'
 
 export default class UserDAO extends BaseDAO<User> {
-    constructor() { 
-        super(TABLE_NAME)
-        this.$init()
-    }
+  constructor() {
+    super(TABLE_NAME)
+    this.$init()
+  }
 
-    private async $init() {
-        try {
-            const db = SQLiteDB.getInstance()
-            await db.execAsync(
-                [{ sql: `create table if not exists ${TABLE_NAME} 
+  private async $init() {
+    try {
+      const db = SQLiteDB.getInstance()
+      await db.execAsync(
+        [
+          {
+            sql: `create table if not exists ${TABLE_NAME} 
                     (id integer primary key autoincrement not null, name text, email text);`,
-                args: []}], false)
-        } catch(err) {
-            if (err instanceof Error) throw err
-            throw new Error('Error al iniciar la tabla')
-        }
-        // .catch(err => console.error('Error al instanciar la tabla', err))
+            args: []
+          }
+        ],
+        false
+      )
+    } catch (err) {
+      if (err instanceof Error) throw err
+      throw new Error('Error al iniciar la tabla')
     }
+    // .catch(err => console.error('Error al instanciar la tabla', err))
+  }
 
-    async insertOne(data: Omit<User, 'id'>) {
-        try {
-            const db = SQLiteDB.getInstance()
-            const res = await db.execAsync([
-                { sql: `insert into ${TABLE_NAME} (email, name) values (?, ?);`, args: [data.email, data.name] }
-            ], false)
-            return res[0].insertId
-        } catch(err) {
-            if(err instanceof Error) throw err
-            throw new Error('Failed to create User')
-        }
+  async insertOne(data: Omit<User, 'id'>) {
+    try {
+      const db = SQLiteDB.getInstance()
+      const res = await db.execAsync(
+        [
+          {
+            sql: `insert into ${TABLE_NAME} (email, name) values (?, ?);`,
+            args: [data.email, data.name]
+          }
+        ],
+        false
+      )
+      return res[0].insertId
+    } catch (err) {
+      if (err instanceof Error) throw err
+      throw new Error('Failed to create User')
     }
+  }
 }
