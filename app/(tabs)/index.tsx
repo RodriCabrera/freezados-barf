294fdd04b7b65f9d6970acff8af963f94ca091a0
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-08 15:07:48 -0300

    remove EditScreenInfo component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../../components/EditScreenInfo'
 import { Text, View } from '../../components/Themed'
 
 import { DAOTests } from '../../components/DAOTests'
@@ -8,14 +7,13 @@ import { DAOTests } from '../../components/DAOTests'
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Home</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
       <DAOTests />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   )
 }
