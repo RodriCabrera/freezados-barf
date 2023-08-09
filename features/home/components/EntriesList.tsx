5955023a0e167aa72e350a1686575b0d15732d4f
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-09 18:54:20 -0300

    Refactor EntriesList using FlatList

--- features/home/components/EntriesList.tsx
+++ features/home/components/EntriesList.tsx
@@ -1,30 +1,24 @@
-import { StyleSheet } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 
-import { Text, View } from '../../common/components/Themed'
+import { View } from '../../common/components/Themed'
 import { useEntries } from '../hooks/useEntries'
 import { Entry } from './Entry'
 
 export const EntriesList = () => {
   const { allEntries } = useEntries()
-
   return (
     <View style={styles.container}>
-      <View style={styles.sliderContainer}>
-        <Text>Slider</Text>
-      </View>
-      {allEntries?.map((entry) => <Entry key={entry.id} entry={entry} />)}
+      <FlatList
+        data={allEntries}
+        renderItem={({ item }) => <Entry key={item.id} entry={item} />}
+        keyExtractor={(item) => `${item.id}`}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'yellow',
     gap: 15
-  },
-  sliderContainer: {
-    backgroundColor: 'gray',
-    display: 'flex',
-    height: 40
   }
 })
