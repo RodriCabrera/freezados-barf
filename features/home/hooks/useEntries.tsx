c2e4457b849340812d355e57f61a8482496d0514
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-09 15:29:28 -0300

    create custom hook to get entries

---
+++ features/home/hooks/useEntries.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+import EntryDAO, { type Entry } from '../../../database/DAOs/Entry.dao'
+import FoodDAO, { type Food } from '../../../database/DAOs/Food.dao'
+
+export const useEntries = () => {
+  const entries = new EntryDAO()
+  const food = new FoodDAO()
+  const [allEntries, setAllEntries] =
+    useState<Array<Entry & { food_name: string | undefined }>>()
+  const [allFoods, setAllFoods] = useState<Food[]>()
+
+  useEffect(() => {
+    food.getAll().then((res) => {
+      setAllFoods(res)
+    })
+  }, [])
+
+  useEffect(() => {
+    entries.getAll().then((res) => {
+      const parsedEntries = res?.map((entry) => {
+        const food = allFoods?.find((food) => food.id === entry.food_id)
+        return {
+          ...entry,
+          food_name: food?.name
+        }
+      })
+      setAllEntries(parsedEntries)
+    })
+  }, [allFoods])
+
+  return { allEntries }
+}
