d25e8652fd5cc1e5f04b27038bb8edc4cb8237a0
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-09 17:34:43 -0300

    Refactor common Icon

---
+++ features/common/components/Icon.tsx
@@ -0,0 +1,13 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+// MaterialCommunityIcons list: https://icons.expo.fyi/
+
+interface IconProps {
+  name: React.ComponentProps<typeof MaterialCommunityIcons>['name']
+  color: string
+  size?: number
+}
+
+export const Icon = ({ name, color, size }: IconProps) => (
+  <MaterialCommunityIcons size={size ?? 28} name={name} color={color} />
+)
