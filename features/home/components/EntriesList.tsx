8ce93d8cf9a92976eb78105097201419060ce92a
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-09 21:31:11 -0300

    remove custom hook

--- features/home/components/EntriesList.tsx
+++ features/home/components/EntriesList.tsx
@@ -1,11 +1,42 @@
 import { FlatList, StyleSheet } from 'react-native'
+import { useEffect, useState } from 'react'
 
 import { View } from '../../common/components/Themed'
-import { useEntries } from '../hooks/useEntries'
+import EntryDAO, {
+  type Entry as EntryType
+} from '../../../database/DAOs/Entry.dao'
+import FoodDAO, { type Food } from '../../../database/DAOs/Food.dao'
 import { Entry } from './Entry'
 
 export const EntriesList = () => {
-  const { allEntries } = useEntries()
+  const entries = new EntryDAO()
+  const food = new FoodDAO()
+  const [allEntries, setAllEntries] =
+    useState<Array<EntryType & { food_name: string | undefined }>>()
+  const [allFoods, setAllFoods] = useState<Food[]>()
+
+  useEffect(() => {
+    food.getAll().then((res) => {
+      setAllFoods(res)
+    })
+  }, [])
+
+  useEffect(() => {
+    entries.getAll().then((res) => {
+      const entriesWithFoodName = res?.map((entry) => {
+        const food = allFoods?.find((food) => food.id === entry.food_id)
+        return {
+          ...entry,
+          food_name: food?.name
+        }
+      })
+      const notTakenEntries = entriesWithFoodName?.filter(
+        (entry) => !entry.taken
+      )
+      setAllEntries(notTakenEntries)
+    })
+  }, [allFoods])
+
   return (
     <View style={styles.container}>
       <FlatList
