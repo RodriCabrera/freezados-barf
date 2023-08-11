e121b6de82acc45559f1f473676c1f16d72c970b
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-08-11 00:52:01 -0300

    change to deconstructed entry objects

--- features/home/components/Entry.tsx
+++ features/home/components/Entry.tsx
@@ -20,21 +20,18 @@ export const Entry = ({ entry }: EntryParams) => {
   const { Food, Ubication, id, quantity, date_ready, date_stored } = entry
   return (
     <View style={styles.container}>
-      <Icon name={ICON_MAP[entry.Food.species]} />
-      <Text key={entry.id} style={styles.header}>
-        {entry.Food.name} {entry.quantity}gr
+      <Icon name={ICON_MAP[Food.species]} />
+      <Text key={id} style={styles.header}>
+        {Food.name} {quantity}gr
       </Text>
       <Text>
         <Icon name="calendar" />
-        {entry.date_stored && new Date(entry.date_stored).toLocaleDateString()}
+        {date_stored && new Date(date_stored).toLocaleDateString()}
       </Text>
-      <Text>In {entry.Ubication.name}</Text>
+      <Text>In {Ubication.name}</Text>
+      <Text>UbicationType: {Ubication.isFreezer ? 'other' : 'freezer'}</Text>
       <Text>
-        UbicationType: {entry.Ubication.isFreezer ? 'other' : 'freezer'}
-      </Text>
-      <Text>
-        date_ready:{' '}
-        {entry.date_ready && new Date(entry.date_ready).toLocaleDateString()}
+        date_ready: {date_ready && new Date(date_ready).toLocaleDateString()}
       </Text>
     </View>
   )
