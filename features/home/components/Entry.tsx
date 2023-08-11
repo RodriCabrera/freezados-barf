98548d04b351f5ec857552a3300979d665b7fd0e
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-08-11 00:50:16 -0300

    add some icons and data display to entry list item

--- features/home/components/Entry.tsx
+++ features/home/components/Entry.tsx
@@ -1,24 +1,41 @@
 import { StyleSheet } from 'react-native'
 
+import { Icon, type IconProps } from '../../common/components/Icon'
 import { Text, View } from '../../common/components/Themed'
 import { type EntryFull } from '../../../database/DAOs/Entry.dao'
+import { type Food } from '../../../database/DAOs/Food.dao'
 
 interface EntryParams {
   entry: EntryFull
+}
+
+const ICON_MAP: Record<Food['species'], IconProps['name']> = {
+  AVIAR: 'food-drumstick',
+  BOVINE: 'cow',
+  FISH: 'fish',
+  PIG: 'pig-variant'
 }
 
 export const Entry = ({ entry }: EntryParams) => {
   const { Food, Ubication, id, quantity, date_ready, date_stored } = entry
   return (
     <View style={styles.container}>
-      <Text key={id} style={styles.header}>
-        {Food.name} ({Food.species}) {quantity}gr
+      <Icon name={ICON_MAP[entry.Food.species]} />
+      <Text key={entry.id} style={styles.header}>
+        {entry.Food.name} {entry.quantity}gr
       </Text>
-      <Text>date_stored: {date_stored}</Text>
-      <Text>UbicationID: {Ubication.id}</Text>
-      <Text>UbicationName: {Ubication.name}</Text>
-      <Text>UbicationType: {Ubication.isFreezer ? 'other' : 'freezer'}</Text>
-      <Text>date_ready: {date_ready}</Text>
+      <Text>
+        <Icon name="calendar" />
+        {entry.date_stored && new Date(entry.date_stored).toLocaleDateString()}
+      </Text>
+      <Text>In {entry.Ubication.name}</Text>
+      <Text>
+        UbicationType: {entry.Ubication.isFreezer ? 'other' : 'freezer'}
+      </Text>
+      <Text>
+        date_ready:{' '}
+        {entry.date_ready && new Date(entry.date_ready).toLocaleDateString()}
+      </Text>
     </View>
   )
 }
