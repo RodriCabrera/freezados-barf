553d21490144f4a58485c2d3e33996d7584f1f05
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-08-11 12:22:40 -0300

    add minimal styling to entry item

--- features/home/components/Entry.tsx
+++ features/home/components/Entry.tsx
@@ -16,35 +16,52 @@ const ICON_MAP: Record<Food['species'], IconProps['name']> = {
   PIG: 'pig-variant'
 }
 
+const dateOptions: Intl.DateTimeFormatOptions = {
+  day: 'numeric',
+  month: 'short'
+}
+
 export const Entry = ({ entry }: EntryParams) => {
   const { Food, Ubication, id, quantity, date_ready, date_stored } = entry
   return (
     <View style={styles.container}>
-      <Icon name={ICON_MAP[Food.species]} />
+      <View style={styles.icon}>
+        <Icon name={ICON_MAP[Food.species]} />
+      </View>
       <Text key={id} style={styles.header}>
         {Food.name} {quantity}gr
       </Text>
-      <Text>
-        <Icon name="calendar" />
-        {date_stored && new Date(date_stored).toLocaleDateString()}
-      </Text>
-      <Text>In {Ubication.name}</Text>
-      <Text>UbicationType: {Ubication.isFreezer ? 'other' : 'freezer'}</Text>
-      <Text>
-        date_ready: {date_ready && new Date(date_ready).toLocaleDateString()}
-      </Text>
+      <View>
+        <Text>
+          {date_stored &&
+            new Date(date_stored).toLocaleDateString(undefined, dateOptions)}
+        </Text>
+        <Text>In {Ubication.name}</Text>
+        <Text>UbicationType: {Ubication.isFreezer ? 'other' : 'freezer'}</Text>
+        <Text>
+          date_ready:{' '}
+          {date_ready &&
+            new Date(date_ready).toLocaleDateString(undefined, dateOptions)}
+        </Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'pink'
+    backgroundColor: 'pink',
+    flexDirection: 'row',
+    gap: 1,
+    padding: 5
   },
   header: {
     color: 'red',
     fontSize: 20,
-    fontWeight: 'bold',
-    width: '100%'
+    fontWeight: 'bold'
+  },
+  icon: {
+    backgroundColor: 'transparent',
+    padding: 5
   }
 })
