5034a35c679a35564975a16978f191c820dfd89b
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-12 19:29:25 -0300

    clean up code

--- database/SQLite.database.ts
+++ database/SQLite.database.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-extraneous-class */
 import * as SQLite from 'expo-sqlite'
 
-const database: SQLite.SQLiteDatabase = SQLite.openDatabase('freezados.db')
+const database = SQLite.openDatabase('freezados.db')
 
 export default database
